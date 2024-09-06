10f9ff60d78096e217eb9650dc79ac0fc14d7390
kamranBaloch1 <kamranbalochhh18@gmail.com>
2024-09-06 05:07:17 -0700

    first commit

---
+++ util/firbaseConfig.ts
@@ -0,0 +1,14 @@
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA6QpByjtYQql19Fv_vJmPedisfqFymwl0",
+  authDomain: "next-instagram-8a1aa.firebaseapp.com",
+  projectId: "next-instagram-8a1aa",
+  storageBucket: "next-instagram-8a1aa.appspot.com",
+  messagingSenderId: "952382745870",
+  appId: "1:952382745870:web:750f568b5c3322898ffb5f"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export default app;
